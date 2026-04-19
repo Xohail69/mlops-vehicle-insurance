b61246585e71e185d89dc7985966838e4b09c08f
Sohail Rayeen <sohailrayeen0786@gmail.com>
2026-04-19 15:53:23 +0530

    feat: initialize project configuration with pyproject.toml and setup.py files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sohail Rayeen",
+    author_email="sohailrayeen0786@gmail.com",
+    packages=find_packages()
+)
